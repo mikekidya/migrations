395d6a7daf3d601cab2d534d21397b904de22eea
Mikhail Kidyankin <kidyankin@icloud.com>
2020-04-05 15:09:49 +0300

    Add docs to migration method

--- src/main/java/migrations/Migration.java
+++ src/main/java/migrations/Migration.java
@@ -7,6 +7,13 @@ import static migrations.api.OldStorageAPI.oldStorageAPI;
 
 public class Migration {
 
+    /**
+     * Transfers file from old storage to new one.
+     * File will bw deleted from old storage after
+     * successful transfer.
+     *
+     * @param filename name of file to be transferred.
+     */
     public static void migrateFile(String filename) {
         while (!newStorageAPI().getFiles().contains(filename)) {
             InputStream file = oldStorageAPI().getFile(filename);
